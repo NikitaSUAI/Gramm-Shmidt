ad3f8e6dd6abd471efc0fe8ddd1bd7739d584b15
Nikittossii <Nikitasobakabmx@gmail.com>
2021-07-26 07:30:45 +0300

    tests?

--- src/main.cc
+++ src/main.cc
@@ -8,8 +8,12 @@
 #include "core/shop.h"
 
 
-int main() {
-	Reader r("/home/nikittossii/Documents/AliceShop/input_file.txt");
+int main(int argc, char** argv) {
+	std::string file_path = "../input_file.txt";
+	if(2 == argc){
+		file_path = argv[1];
+	}
+	Reader r(file_path);
 	auto v1 = r.ReadDB();
 	auto v2 = r.ReadInstances();
 	Shop shop(v1);
